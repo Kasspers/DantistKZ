390795a711606a80df31318041388d8e332761f6
stablik <stablik@bk.ru>
2016-01-27 12:11:30 +0600

    1st
    1st commit

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DantistKZ
+//
+//  Created by stablik on 27.01.16.
+//  Copyright © 2016 stablik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
